56f3e248e0f1f54e77976683d6748ffe3f62ead0
Naxaur <c1nacgab@vasvari.org>
2026-02-03 10:39:06 +0100

    employee adószáma és ki nem adott szabadság napjainak száma megjelenítése a main-en

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -3,5 +3,10 @@
     public class EmployeeViewModel
     {
         public string Name { get; } = "Nagy Anna";
+
+        public string AdoSzam { get; } = "12345678-1-13";
+
+        public string KiNemAdottSzabadsag { get; } = "10";
+
     }
 }
