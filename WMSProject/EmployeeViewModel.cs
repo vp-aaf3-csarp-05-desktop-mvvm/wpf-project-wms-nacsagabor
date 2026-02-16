1a1ee9956392bd34446a2c16429e0696433f9d45
nacsa <c1nacgab@vasvari.org>
2026-02-16 20:04:26 +0100

    éves fizetés kiszámítása. (önálló feladat)

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -8,5 +8,15 @@
 
         public string KiNemAdottSzabadsag { get; } = "10";
 
+        /// <summary>
+        /// Éves fizetés kiszámítása
+        /// </summary>
+
+        public int HetiFutarFizetes { get; } = 15000;
+
+        public int Osztondij { get; } = 42000;
+
+        public int EvesFizetes => ((HetiFutarFizetes * 4) + Osztondij) * 12 ;
+
     }
 }
