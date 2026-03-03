92adeb9d777be5f255cffb80bcd5c6ec73f0e60c
Naxaur <c1nacgab@vasvari.org>
2026-03-03 10:23:48 +0100

    tovabbfejlesztés, kereset kiiratása kiszámítása

--- WMSProject/EmployeeViewModel.cs
+++ WMSProject/EmployeeViewModel.cs
@@ -2,12 +2,6 @@
 {
     public class EmployeeViewModel
     {
-        public string Name { get; } = "Nagy Anna";
-
-        public string AdoSzam { get; } = "12345678-1-13";
-
-        public string KiNemAdottSzabadsag { get; } = "10";
-
         /// <summary>
         /// Éves fizetés kiszámítása
         /// </summary>
@@ -16,7 +10,7 @@
 
         public int Osztondij { get; } = 42000;
 
-        public int EvesFizetes => ((HetiFutarFizetes * 4) + Osztondij) * 12 ;
+        public int EvesFizetes => ((HetiFutarFizetes * 4) + Osztondij) * 12;
 
     }
 }
